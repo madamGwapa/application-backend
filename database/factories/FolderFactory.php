d39dc7edb09a41844b062b9f52148f8181b6b4c0
blackDragon123 <earlbulasa@gmail.com>
2017-10-24 23:02:20 +0800

    unique

--- database/factories/FolderFactory.php
+++ database/factories/FolderFactory.php
@@ -4,15 +4,15 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Folder::class, function (Faker $faker) {
     return [
-        'code' => $faker->random()->randomLetter,
-        'folder_name' => $faker->random()->word
+        'code' => $faker->unique()->randomLetter,
+        'folder_name' => $faker->word
     ];
 });
 
 $factory->define(App\Workpaper::class, function (Faker $faker) {
     return [
         //
-        'reference' => $faker->word,
+        'reference' => $faker->unqique()->word,
         'content' => $faker->sentence,
         'folder_id' => function () {
             return factory('App\Folder')->create()->id;
