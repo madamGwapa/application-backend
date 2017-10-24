2c63f8f8d8231f64b2e863923d8c911aba10b41d
blackDragon123 <earlbulasa@gmail.com>
2017-10-24 22:41:34 +0800

    factory

---
+++ database/factories/FolderFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Folder::class, function (Faker $faker) {
+    return [
+        'code' => $faker->random()->randomLetter,
+        'folder_name' => $faker->random()->word;
+    ];
+});
+
+$factory->define(App\Workpaper::class, function (Faker $faker) {
+    return [
+        //
+        'reference' => $faker->word,
+        'content' => $faker->sentence,
+        'folder_id' => function () {
+            return factory('App\Folder')->create()->id;
+        }
+    ];
+});
