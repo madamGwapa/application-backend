c86822c239c21911db18d598345a7e4fc06ceae2
blackDragon123 <earlbulasa@gmail.com>
2017-10-24 17:55:42 +0800

    test update

--- tests/Feature/FolderTest.php
+++ tests/Feature/FolderTest.php
@@ -14,13 +14,14 @@ class FolderTest extends TestCase
      *
      * @return void
      */
+     /** @test */
      public function test_add_folder(){
        $folder = factory('App\Folder')->make();
        $response = $this->post('/api/folder', $folder->toArray());
        $response->assertStatus(200);
        $response->assertSee('Folder Succesfully Added');
      }
-
+     /** @test */
      public function test_update_folder()
      {
         $folder = factory('App\Folder')->create();
@@ -29,14 +30,14 @@ class FolderTest extends TestCase
         $response->assertStatus(200);
         $response->assertSee('Folder Succesfully Updated');
       }
-
+      /** @test */
       public function test_delete_folder(){
         $folder = factory('App\Folder')->create();
         $response = $this->delete('/api/folder/'.$folder->id);
         $response->assertStatus(200);
         $response->assertSee('Folder Succesfully Deleted');
       }
-
+      /** @test */
       public function test_view_folders()
       {
         $folder = factory('App\Folder', 10)->create();
@@ -45,7 +46,7 @@ class FolderTest extends TestCase
         $response->assertStatus(200);
         $response->assertSee($json);
       }
-
+      /** @test */
       public function test_view_folder_papers(){
         $folder = factory('App\Folder')->create();
         factory('App\WorkPaper', 10, ['folder_id' => $folder->id])->create();
--- tests/Feature/WorkpaperTest.php
+++ tests/Feature/WorkpaperTest.php
@@ -13,13 +13,14 @@ class WorkpaperTest extends TestCase
      *
      * @return void
      */
+     /** @test */
      public function test_add_paper(){
        $paper = factory('App\Workpaper')->make();
        $response = $this->post('/api/work-paper', $folder->toArray());
        $response->assertStatus(200);
        $response->assertSee('Workpaper Succesfully Added');
      }
-
+     /** @test */
      public function test_update_paper()
      {
         $paper = factory('App\Workpaper')->create();
@@ -28,14 +29,14 @@ class WorkpaperTest extends TestCase
         $response->assertStatus(200);
         $response->assertSee('Folder Succesfully Updated');
       }
-
+      /** @test */
       public function test_delete_paper(){
         $folder = factory('App\Workpaper')->create();
         $response = $this->delete('/api/work-paper/'.$folder->id);
         $response->assertStatus(200);
         $response->assertSee('Workpaper Succesfully Deleted');
       }
-
+      /** @test */
       public function test_view_papers()
       {
         $folder = factory('App\Workpaper', 10)->create();
